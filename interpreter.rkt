ca9cf02cccf927482bb029f94509a7984cdf267d
Benjamin Zheng <54424912+ZhengBenjamin@users.noreply.github.com>
2025-02-24 15:53:19 -0500

    fixed setting states

--- interpreter.rkt
+++ interpreter.rkt
@@ -17,23 +17,19 @@
 (define state '(() ())) 
 
 (define append_state 
-  (lambda (var val)
-    (append_var var (car state))
-    (append_val val (cdr state))))
+  (lambda (var val old_state)
+    (cons (append_var var (car old_state))
+          (append_val val (cdr old_state)))))
 
 ; Appends a value to the value list within state
 (define append_val
   (lambda (val val_list)
-      (if (pair? val_list) 
-        (cons (car val_list) (append_val val (cdr val_list)))
-        (cons val val_list))))
+    (cons var var_list)))
 
 ; Appends a variable to the variable list within state
 (define append_var
   (lambda (var var_list)
-    (if (pair? var_list)
-      (cons (car var_list) (append_var var (cdr var_list)))
-      (cons var var_list))))
+    (cons val val_list)))
 
-; test
-
+; 
+ 
