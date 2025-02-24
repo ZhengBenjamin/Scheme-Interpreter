fc6843ebdb3288e31a0732a94bef9a6309226ef6
ZhengBenjamin <zhengbenjamin1218@gmail.com>
2025-02-24 16:59:20 -0500

    Added some stuff

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require "simpleParser.rkt")
 (provide (all-defined-out))
-;===============================================================
+;=======================================
 ;; Interpreter 
 ;; Tyler Powers, Henry Odza, and Benjamin Zheng 
 ;;
@@ -12,6 +12,23 @@
 ; Input: input file with code
 (define interpret (lambda (input) (parser input)))
 
+; Declares a new variable with no value 
+; Returns state with var added to it
+; TODO: Check if var already exists
+(define var_dec
+  (lambda (var state)
+    (append_state var null state)))
+
+; Declares a new variable with a value 
+(define var_dec_assn
+  (lambda (var val state) 
+    (append_state var val state)))
+
+
+;=======================================
+;; State Logic 
+;=======================================
+
 ; The state of the interpreter. Starts empty
 ; Format (var_list val_list)
 (define init_state '(() ()))
@@ -20,7 +37,7 @@
 (define append_state 
   (lambda (var val old_state)
     (cons (append_var var (car old_state))
-      (append_val val (cdr old_state)))))
+          (list (append_val val (car (cdr old_state)))))))
 
 ; Appends a value to the value list within state
 (define append_var
@@ -31,6 +48,25 @@
 (define append_val
   (lambda (val val_list)
     (cons val val_list)))
+
+; Checks if a variable exists in the state
+; Returns true if it exists, false otherwise
+(define var_exists? 
+  (lambda (var state)
+    (cond 
+      ((null? (car state)) #f)
+      ((equal? var (car (car state))) #t)
+      (else (var_exists? var (cons (cdr (car state)) (cdr state)))))))
+
+; Checks if a variable has been initialized with a value
+; Returns true if it has been initialized, false otherwise
+(define var_init?
+  (lambda (var state)
+    (cond
+      ((eq? #f (var_exists? var state)) #f)
+      ((null? (cdr state)) #f)
+      ((equal? var (car (car state))) (not (null? (car (cdr state)))))
+      (else (var_init? var (cons (cdr (car state)) (cdr (cdr state))))))))
 
 (define add
   (lambda (x y)
